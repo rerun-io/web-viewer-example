0fb5c20a291024cfb827005fcba85baf630faf16
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-11-28 12:02:31 +0100

    add back top level await plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 /** @type {import("vite").UserConfig} */
 const config = {
-  plugins: [wasm()],
+  plugins: [wasm(), topLevelAwait()],
 };
 
 if ("REPOSITORY" in process.env) {
