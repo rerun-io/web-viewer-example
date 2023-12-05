577fd583892b6a3e433233d911901a717a389582
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-12-05 10:38:04 +0100

    update versions

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import "./style.css";
 import { WebViewer } from "@rerun-io/web-viewer";
 
+const rrd =
+  new URLSearchParams(location.search).get("url") ||
+  "https://demo.rerun.io/version/0.11.0/examples/structure_from_motion/data.rrd";
 const viewer = new WebViewer();
-viewer.start("https://demo.rerun.io/version/nightly/examples/structure_from_motion/data.rrd");
+viewer.start(rrd);
 
