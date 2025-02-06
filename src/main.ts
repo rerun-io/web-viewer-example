e285c219bc6281906267dbdac729b240776e89bd
jprochazk <pjanv42@gmail.com>
2025-02-06 17:24:01 +0100

    Update web-viewer to 0.22

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { WebViewer } from "@rerun-io/web-viewer";
 
 const rrd =
   new URLSearchParams(location.search).get("url") ||
-  "https://app.rerun.io/version/0.20.3/examples/structure_from_motion.rrd";
+  "https://app.rerun.io/version/0.22.0/examples/structure_from_motion.rrd";
 const viewer = new WebViewer();
 viewer.start(rrd, null, {
   width: "100%",
